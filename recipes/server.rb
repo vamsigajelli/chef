aa22dbc2aecc1e5fba76a2ef10d08fd49f7a9aa8
vagrant <vagrant@localhost.localdomain>
2019-03-13 04:40:34 +0000

    changes made to metadata file

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,7 +9,12 @@ action :install
 end
 
 file '/var/www/html/index.html' do
-content '<h1>hello world!</h1>'
+content "<h1>hello world!</h1>
+HOSTNAME: #{node['hostname']}
+IPaddress: #{node['ipaddress']}
+CPU: #{node['cpu']['0']['mhz']}
+MEMORY: #{node['memory']['total']}
+"
 end
 
 service 'httpd' do
