e80dc41741df8474a7c7bc40b88b3b87585153e1
vagrant <vagrant@localhost.localdomain>
2019-03-15 04:35:48 +0000

    changes made

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,6 +8,10 @@ package 'httpd' do
 action :install
 end
 
+remote_file 'var/www/html/vamsi.jpg' do
+source 'https://upload.wikimedia.org/wikipedia/commons/1/12/VIRAT_KOHLI_JAN_2015_%28cropped%29.jpg'
+end
+
 template '/var/www/html/index.html' do
 source 'index.html.erb'
 action :create
